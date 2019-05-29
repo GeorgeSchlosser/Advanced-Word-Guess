80a2d267d5e1940162ca7bc65c430dbd21f58bc2
Trash Panda <d.george.schlosser@gmail.com>
2019-05-29 03:46:27 -0400

    index begun, requirements and global variables added

---
+++ index.js
@@ -0,0 +1,21 @@
+// bring in inquirer & Word.js
+var inquirer = require(inquirer);
+var word = require("./Word.js")
+
+// GLOBAL VARIABLES
+// ===============================================================================
+// possible words to guess
+var wordbank = ["freddy","jason", "redrum", "xenomorph", "cenobite", "necronomicon", "tuttifrutti", "elvira", "romero", "pazuzu", "regan", "witch"];
+// chances to complete the word
+var guesses  = 6;
+// counts completed words
+var wins = 0;
+
+
+// MAIN PROCESSES
+// ===============================================================================
+
+
+
+// FUNCTIONS
+// ===============================================================================
